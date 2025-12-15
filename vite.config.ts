7d03030479560879ae17e08c407f8db0643bf0db
tanyels <103962149+tanyels@users.noreply.github.com>
2025-12-15 16:08:09 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This bridges the gap between node-style process.env and Vite's browser-style import.meta.env
+  define: {
+    'process.env.API_KEY': 'import.meta.env.VITE_API_KEY'
+  },
   build: {
     outDir: 'dist',
   },
